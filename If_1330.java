c42ed77edfce18b2c0a1eec20fff19cd3fc4a90b
luvforjunk <csj3104@naver.com>
2022-03-19 21:51:49 +0900

    1330_if문_두수비교하기

---
+++ If_1330.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int a, b;
+    a = scan.nextInt();
+    b = scan.nextInt();
+     
+    if (a > b){
+      System.out.println(">");
+    } else if (a < b){
+      System.out.println("<");
+    } else {
+    System.out.println("==");
+    }
+  }
+}
