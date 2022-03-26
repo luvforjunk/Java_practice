5f31992f51c21fa9444ccb2ac216576a008108d0
luvforjunk <csj3104@naver.com>
2022-03-26 22:23:16 +0900

    2480_if문_주사위세개_오답

---
+++ If_2480.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int a, b, c;
+
+    a = scan.nextInt();
+    b = scan.nextInt();
+    c = scan.nextInt();
+
+    if(a == b && b == c)
+      System.out.println(10000 + a * 1000);
+    
+    if ((a == b && a != c) || (a != b && a == c))
+      System.out.println(1000 + a * 100);
+    if (a != b && b == c)
+      System.out.println(1000 + b * 100);
+
+    if (a > b && a > c)
+      System.out.println(a*100);
+    if (b > a && b > c)
+      System.out.println(b*100);
+    if (c > a && c > b)
+      System.out.println(c*100);
+  } 
+}
